6cc63a5f5a09b71cc43157c540d7ff10974d0eac
Patrick Gaskin <patrick@pgaskin.net>
2026-08-16 22:56:18 -0400

    Fix SPDX header

--- control/src/main/java/net/pgaskin/remotedesktop/control/playground/Recordings.java
+++ control/src/main/java/net/pgaskin/remotedesktop/control/playground/Recordings.java
@@ -1,5 +1,5 @@
 // SPDX-FileCopyrightText: 2026 Patrick Gaskin
-// SPDX-License-Identifier: GPL-3.0-or-later
+// SPDX-License-Identifier: MIT
 
 package net.pgaskin.remotedesktop.control.playground;
 
